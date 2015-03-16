2968997506631ea3e7b9a9fbf6b71670814ad2c7
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-03-16 23:37:25 +0100

    comment out port no

--- bin/duoshuo-stream.php
+++ bin/duoshuo-stream.php
@@ -11,7 +11,7 @@ require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
 $keyspace = 'feideconnct';
 $nodes = [[ 
 	'host'   		 => '127.0.0.1',
-    'port'   		 => 9042,
+    // 'port'   		 => 9042,
     'class'     	 => 'Cassandra\Connection\Stream',//use stream instead of socket, default socket. Stream may not work in some environment
     'connectTimeout' => 4, // connection timeout, default 5,  stream transport only
     'timeout'   	 => 8 // write/recv timeout, default 30, stream transport only
