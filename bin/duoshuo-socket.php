b317e5476401c94e1c91a586be5cd530fec57850
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-03-16 21:16:49 +0100

    Adding some cassandra test scripts

---
+++ bin/duoshuo-socket.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+namespace FeideConnect;
+
+use FeideConnect\Data\StorageProvider;
+
+require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
+
+
+$keyspace = 'feideconnct';
+$nodes = [
+	'127.0.0.1'
+];
+
+
+
+
+
+echo("connecting to nodes"); 
+$db = new \Cassandra\Connection($nodes, $keyspace);
+
+echo "Constructor done\n";
+$db->connect();
+
+echo "Connected done\n";
+
