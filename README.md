52b00e9e573df2c2fbb10d606f3338d7717377ad
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2015-03-17 08:34:58 +0100

    adding persistent stream test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cassa-test
+
 A few test scripts to test various Cassandra binding-libraries for PHP
---
+++ bin/duoshuo-stream-persistent.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+namespace FeideConnect;
+
+use FeideConnect\Data\StorageProvider;
+
+require(dirname(dirname(__FILE__)) . '/vendor/autoload.php');
+
+
+$keyspace = 'feideconnect';
+$nodes = [[ 
+	'host'   		 => '127.0.0.1',
+    // 'port'   		 => 9042,
+    'class'     	 => 'Cassandra\Connection\Stream',//use stream instead of socket, default socket. Stream may not work in some environment
+    'connectTimeout' => 4, // connection timeout, default 5,  stream transport only
+    'timeout'   	 => 8 // write/recv timeout, default 30, stream transport only
+    'persistent'	=> true
+]];
+
+echo("connecting to nodes"); 
+$db = new \Cassandra\Connection($nodes, $keyspace);
+
+echo "Constructor done\n";
+$db->connect();
+
+echo "Connected done\n";
